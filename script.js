a1eaf4798986087ca5efff8dcd69b8a27572d5ad
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-06-06 23:30:21 +0200

    Added 2nd gen telemetry code (JS)

--- script.js
+++ script.js
@@ -24,7 +24,9 @@ btn.addEventListener("click", function() {
         doNotTrack: null,
         connectionInfo: null,
         deviceMemory: null,
-        hardwareConcurrency: null
+        hardwareConcurrency: null,
+        canvasFingerprint: null,
+        audioContextFingerprint: null
     };
 
     // WebGL Fingerprinting
@@ -32,6 +34,31 @@ btn.addEventListener("click", function() {
     let gl = canvas.getContext('webgl');
     let debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
     data.webGLFingerprint = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+
+    // Improved Canvas Fingerprinting
+    let canvas2 = document.createElement('canvas');
+    let ctx = canvas2.getContext('2d');
+    ctx.fillStyle = 'rgba(75, 13, 160, 0.6)';
+    ctx.fillRect(50, 50, 100, 100);
+    ctx.rotate(0.5);
+    ctx.font = '60px Arial';
+    ctx.fillText('Hello, world!', 50, 50);
+    data.canvasFingerprint = canvas2.toDataURL();
+
+    // Improved AudioContext Fingerprinting
+    let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    let oscillator = audioContext.createOscillator();
+    let analyser = audioContext.createAnalyser();
+    oscillator.type = 'sawtooth';
+    oscillator.frequency.setValueAtTime(500, audioContext.currentTime); // value in hertz
+    oscillator.connect(analyser);
+    analyser.connect(audioContext.destination);
+    oscillator.start(0);
+    analyser.fftSize = 2048;
+    let bufferLength = analyser.frequencyBinCount;
+    let dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray);
+    data.audioContextFingerprint = dataArray.join('');
 
     // Ad Blocker detection
     let test = document.createElement('div');
